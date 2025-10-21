1e6ead110013bd3d52314700ed914b2b52504d4e
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2025-10-22 01:42:35 +0200

    ci: tweak pipeline [skip ci]

--- release.config.cjs
+++ release.config.cjs
@@ -4,30 +4,74 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'tweak', release: 'patch' }
+        ]
+      }
+    ],
+    // --------------------
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: '✨ Features' },
+            { type: 'fix', section: '🐛 Bug Fixes' },
+            { type: 'perf', section: '⚡ Performance Improvements' },
+            { type: 'revert', section: '↩️ Reverts' },
+            { type: 'tweak', section: '⚙️ Tweaks', hidden: false },
+            { type: 'docs', section: '📝 Documentation', hidden: true },
+            { type: 'style', section: '💈 Styles', hidden: true },
+            { type: 'chore', section: '🧹 Miscellaneous Chores', hidden: true },
+            { type: 'refactor', section: '🪄 Code Refactoring', hidden: true },
+            { type: 'test', section: '✅ Tests', hidden: true },
+            { type: 'ci', section: '🔁 Continuous Integration', hidden: true },
+          ],
+        },
+      },
+    ],
+    // --------------------
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    // --------------------
     './update-version.js',
+    // --------------------
     [
       '@semantic-release/git',
       {
-        assets: ['gradle.properties'],
-        message: 'chore(release): update version for ${nextRelease.version} [skip ci]',
+        assets: [
+          'gradle.properties',
+          'CHANGELOG.md',
+        ],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
+    // --------------------
     [
       '@semantic-release/exec',
       {
-        prepareCmd: './gradlew build',
+        prepareCmd: './gradlew build --build-cache',
       },
     ],
+    // --------------------
     [
       '@semantic-release/github',
       {
-        'assets': [
+        assets: [
           'build/libs/!(*-@(dev|sources|javadoc)).jar',
         ],
       },
     ],
+    // --------------------
     'semantic-release-export-data',
   ],
 };
